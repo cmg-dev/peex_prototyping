0aee7613f978fd3f6a8714fe110fd096ef12a4d1
Christoph Gnip <christoph.gnip@gmail.com>
2019-11-12 01:29:40 +0100

    :sparkles: Setup app for token usage

--- mix.exs
+++ mix.exs
@@ -14,7 +14,9 @@ defmodule Peex.MkI.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Peex.Processtoken.Application, []}
+
     ]
   end
 
