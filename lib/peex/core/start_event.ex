14ad6e24c5bea769e6a181ef056367d3e77198a2
Christoph Gnip <christoph.gnip@gmail.com>
2019-11-12 15:29:16 +0100

    :sparkles: Use Persistence in StartEvent

--- lib/peex/core/start_event.ex
+++ lib/peex/core/start_event.ex
@@ -9,6 +9,9 @@ defmodule Peex.Core.StartEvent do
   end
 
   def handle_cast({:on_enter, token}, state) do
+    token = persist_on_start(token, state, "")
+
+    Logger.debug "#{__MODULE__} Using token -> #{inspect(token)}"
 
     next_node_id = state.next_node_id
 
@@ -17,6 +20,19 @@ defmodule Peex.Core.StartEvent do
 
     try_cast(next_node_id, {:on_enter, token})
 
+    _persist_on_exit(token, state)
+
     {:noreply, state}
+  end
+
+  defp persist_on_start(token, state, "") do
+    token = %{ token | payload: token.payload,
+      flow_node_instance_id: state.instance_id,
+      parent_caller_instance_id: nil
+    }
+
+    Logger.debug "persist_on_start #{inspect(token)}"
+
+    Repo.insert!(token)
   end
 end
