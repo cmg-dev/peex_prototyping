457135c4177601acfc2a0559bf312c5b8b1a48a4
Christoph Gnip <christoph.gnip@gmail.com>
2019-11-12 15:26:04 +0100

    :sparkles: Improve schema for Processtoken

--- lib/peex/contracts/processtoken.ex
+++ lib/peex/contracts/processtoken.ex
@@ -1,18 +1,23 @@
-defmodule Peex.Processtoken do
+defmodule Contracts.Processtoken do
   use Ecto.Schema
 
   import Ecto.Changeset
 
-  schema "processtoken" do
-    field :process_instance_id, Ecto.UUID, default: Ecto.UUID.generate()
+  schema "processtokens" do
+    field :process_instance_id, Ecto.UUID, default: to_string(Ecto.UUID.generate())
     field :process_model_id, :string
     field :correlation_id, :string
-    field :created_at, :utc_datetime, default: DateTime.utc_now()
-    field :updated_at, :utc_datetime, default: DateTime.utc_now()
+    field :created_at, :utc_datetime, default: DateTime.truncate(DateTime.utc_now, :second)
+    field :updated_at, :utc_datetime, default: DateTime.truncate(DateTime.utc_now, :second)
     field :flow_node_instance_id, :string
     field :identity, :string
     field :parent_caller_instance_id, Ecto.UUID
-    field :payload, :map, default: {}
+    field :payload, :map, default: %{}
+  end
+
+  def changeset(token, params \\ %{}) do
+    token
+    |> cast(params, [:flow_node_instance_id, :parent_caller_instance_id, :payload])
   end
 
 end
