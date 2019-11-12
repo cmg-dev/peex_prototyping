0cbcab27995205234a0229cdf34ee4b22200a73b
Christoph Gnip <christoph.gnip@gmail.com>
2019-11-12 17:07:37 +0100

    :sparkles: Improve the Processtoken model; add flow_node_id

--- lib/peex/contracts/processtoken.ex
+++ lib/peex/contracts/processtoken.ex
@@ -3,6 +3,8 @@ defmodule Contracts.Processtoken do
 
   import Ecto.Changeset
 
+  @changable_fields [:flow_node_instance_id, :flow_node_id, :parent_caller_instance_id, :payload]
+
   schema "processtokens" do
     field :process_instance_id, Ecto.UUID, default: to_string(Ecto.UUID.generate())
     field :process_model_id, :string
@@ -10,6 +12,7 @@ defmodule Contracts.Processtoken do
     field :created_at, :utc_datetime, default: DateTime.truncate(DateTime.utc_now, :second)
     field :updated_at, :utc_datetime, default: DateTime.truncate(DateTime.utc_now, :second)
     field :flow_node_instance_id, :string
+    field :flow_node_id, :string
     field :identity, :string
     field :parent_caller_instance_id, Ecto.UUID
     field :payload, :map, default: %{}
@@ -17,7 +20,7 @@ defmodule Contracts.Processtoken do
 
   def changeset(token, params \\ %{}) do
     token
-    |> cast(params, [:flow_node_instance_id, :parent_caller_instance_id, :payload])
+    |> cast(params, @changable_fields)
   end
 
 end
