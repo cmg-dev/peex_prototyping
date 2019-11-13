ee53ee8eb85ea1a4951656f7668378289f995188
Christoph Gnip <christoph.gnip@gmail.com>
2019-11-13 02:29:28 +0100

    :sparkles: Use persistence in each behavior

--- lib/peex/core/exclusive_join_gateway.ex
+++ lib/peex/core/exclusive_join_gateway.ex
@@ -6,7 +6,11 @@ defmodule Peex.Core.ExclusiveJoinGateway do
 
   def handle_cast({:on_enter, token}, state) do
 
+    _persist_on_enter(token, state, "")
+
     next_node_id = state.next_node_id
+
+    { :ok, token } = _persist_on_exit(token, state, token.payload)
 
     Logger.debug "#{__MODULE__} Starting next -> #{next_node_id}"
     Logger.debug "#{__MODULE__} token: #{inspect(token)}"
--- lib/peex/core/exclusive_split_gateway.ex
+++ lib/peex/core/exclusive_split_gateway.ex
@@ -6,6 +6,8 @@ defmodule Peex.Core.ExclusiveSplitGateway do
 
   def handle_cast({:on_enter, token}, state) do
 
+    _persist_on_enter(token, state, "")
+
     next_nodes = state.next_nodes
 
     evaluation_data = [token: token]
@@ -24,6 +26,8 @@ defmodule Peex.Core.ExclusiveSplitGateway do
 
       Logger.debug "#{__MODULE__} Starting next -> #{next_node_id}"
       Logger.debug "#{__MODULE__} token: #{inspect(token)}"
+
+      { :ok, token } = _persist_on_exit(token, state, token.payload)
 
       try_cast(next_node_id, {:on_enter, token})
 
